659e6a2e39e4b8eb969e311e878a4e4003e06c72
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2018-11-25 17:08:35 +0100

    Add Forms library packages

---
+++ src/GoogleVR.Forms.Droid/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GoogleVR.Forms.Droid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("bitforge AG")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0")]
